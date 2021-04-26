63628a0de649d3d7f41bb9563f080113e0fd0435
Tomas Pinto <tpinto@atixlabs.com>
2021-04-26 12:56:18 -0300

    serve static files (css, etc), add endpoint for testing json

--- myApp.js
+++ myApp.js
@@ -4,14 +4,16 @@ var app = express();
 // app.get("/", function(req, res) {
 //     res.send("Hello Express");
 // })
+app.use('/public', express.static("public"));
 
 app.get("/", function(req, res) {
     fileAbsPath = __dirname + "/views/index.html";
     res.sendFile(fileAbsPath);
 })
 
-
-
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"});
+})
 
 
 
