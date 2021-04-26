e7c8e09e300fc416a16072bab0c109d846236267
Tomas Pinto <tpinto@atixlabs.com>
2021-04-26 11:53:46 -0300

    Serving files

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// })
 
+app.get("/", function(req, res) {
+    fileAbsPath = __dirname + "/views/index.html";
+    res.sendFile(fileAbsPath);
+})
 
 
 
